8540aab61872b2763470d55229f543142fd4b1ae
Aleksandr Nikulenko <11nav09alex1mo@gmail.com>
2019-04-28 03:28:48 +0300

    edit page 2

--- src/New/Components/ProductList/styleIndex.js
+++ src/New/Components/ProductList/styleIndex.js
@@ -18,7 +18,6 @@ const Wrapper = styled(Login)`
     width: 100%;
     height: 87%;
     overflow: auto;
-    /* margin-top: 1rem; */
     box-sizing: border-box;
   }
 
--- src/New/Containers/ProductListContainers/ProductListContainer.js
+++ src/New/Containers/ProductListContainers/ProductListContainer.js
@@ -20,7 +20,7 @@ class ProductListContainer extends Component {
     let wrapper = document.querySelector(".productlist__wrapper");
     let html = document.querySelector("html");
     let fontSize = parseFloat(html.style.fontSize);
-    let width = parseInt(getComputedStyle(wrapper).width);
+    let width = parseInt(getComputedStyle(wrapper).width) - 10;
     let int = Math.floor(width / (320 + 1.1 * fontSize * 2));
     return int * (320 + 1.1 * fontSize * 2);
   };
