ffdbf760288ccb3b25271dbbbaf85ca4ab57965e
Aleksandr Nikulenko <11nav09alex1mo@gmail.com>
2019-04-30 19:40:47 +0300

    add redux

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LoginPageContainer from "./Containers/LoginPageContainers/LoginPageContainer";
 import ProductListContainer from "./Containers/ProductListContainers/ProductListContainer";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension"
+import reduxThunk from "redux-thunk"
+import { applyMiddleware, createStore } from "redux"
+import reducer from "./reducer"
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(reduxThunk)))
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={LoginPageContainer} />
-          <Route exact path="/product-list" component={ProductListContainer} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={LoginPageContainer} />
+            <Route exact path="/product-list" component={ProductListContainer} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- src/Containers/ProductListContainers/ProductListContainer.js
+++ src/Containers/ProductListContainers/ProductListContainer.js
@@ -1,33 +1,19 @@
 import React, { Component } from "react";
 import ProductList from "../../Components/ProductList";
+import { connect } from "react-redux";
+import { fetchProducts } from "../../action"
+
 
 class ProductListContainer extends Component {
   state = {
-    data: null,
     productList: {
       width: null
     }
   };
 
   componentDidMount() {
-    this.getProducts();
+    this.props.fetchProducts();
     this.getWidthForProductList();
-  }
-
-  getProducts() {
-    //http://light-it-04.tk/api/posters/
-    fetch(
-      "https://pixabay.com/api/?key=10255087-ad858ae5e4adfc8ab26a27a9e&q=computer&category=businessi&image_type=photo&per_page=100"
-    )
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        this.setState({
-          data
-        });
-      })
-      .catch(error => console.log(error));
   }
 
   componentWillUnmount() {
@@ -42,7 +28,6 @@ class ProductListContainer extends Component {
       let fontSize = parseFloat(getComputedStyle(body).fontSize);
       let widthWrapper = parseInt(getComputedStyle(wrapper).width) - 10;
       let int = Math.floor(widthWrapper / (fontSize * 1.1 * 2 + 320));
-      console.log(int);
       result = (fontSize * 1.1 * 2 + 320) * int;
       this.setState({
         productList: {
@@ -57,11 +42,22 @@ class ProductListContainer extends Component {
 
   render() {
     let {
-      data,
       productList: { width }
     } = this.state;
+    let { productList } = this.props
+    let data = productList;
+
     return <ProductList products={data && data.hits} width={width} />;
   }
 }
 
-export default ProductListContainer;
+const mapStateToProps = state => ({
+  productList: state.productList
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchProducts: () => dispatch(fetchProducts())
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductListContainer)
